9431454eb24edcf54b3df74edc50fa1f9ab226f1
rapthappynbiang <rapthappynbiang@gmail.com>
2023-03-03 19:28:24 +0530

    chore: add bin folder

---
+++ bin/cli.js
@@ -0,0 +1,28 @@
+const { execSync } = require("child_process");
+
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: `inherit` });
+  } catch (e) {
+    console.log(`Failed to execute ${command}`, e);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckOutCommand = `git clone --depth 1 https://github.com/rapthappynbiang/create-webpack-starter-app.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckOutCommand);
+if (!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+const installDeps = runCommand(installDepsCommand);
+if (!installDeps) process.exit(-1);
+
+console.log(
+  "Successfully Installed dependencies: Follow the following commands to start"
+);
+console.log(`cd ${repoName} && npm start`);
